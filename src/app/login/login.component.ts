657b3f5b32a198f965a363c2dba3aa71844d7e8e
CarsonBain <carsonbain@gmail.com>
2019-08-27 09:33:31 -0700

    feat: login screen

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -12,11 +12,16 @@ export class LoginComponent implements OnInit {
   constructor(public router: Router, public serverService: ServerService) { }
 
   public pinCode = [];
+  public error = false;
 
   ngOnInit() {
   }
 
   public addPin(value: number) {
+    if (this.pinCode.length === 4) {
+      this.pinCode = [];
+    }
+    this.error = false;
     this.pinCode.push(value);
     console.log(this.pinCode);
     if (this.pinCode.length === 4) {
@@ -24,15 +29,18 @@ export class LoginComponent implements OnInit {
     }
   }
 
-  public attemptSubmit() {
-    // maybe pass ID to router?
+  public clearDigit() {
+    this.pinCode.pop();
+    this.error = false;
+  }
+
+  public attemptSubmit(): void{
     const pinCode = this.pinCode.join('');
     const server = this.serverService.authenticateServer(pinCode);
     if (server) {
     this.router.navigate(['/menu', server.id]);
-    console.log('AUTH');
     } else {
-      console.log('False');
+      this.error = true;
     }
 
   }
