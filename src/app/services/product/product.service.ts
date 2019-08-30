88d40a8a1523e65f7f7cca5cc26b98c73a81f1e1
Jonathan Comberbach <jonathan.comberbach@winedirect.com>
2019-08-29 19:23:24 -0700

    in progress

--- src/app/services/product/product.service.ts
+++ src/app/services/product/product.service.ts
@@ -60,12 +60,112 @@ export class ProductService {
       quantity: 0
     },
     {
-      name: 'Red Wine',
+      name: 'Cab Sauv',
       price: 7.99,
       GST: 1.00,
       posCategory: 'Wine',
       GUID: '',
       SKU: 'sku106',
+      selected: false,
+      quantity: 0
+    },
+    {
+      name: 'Pinot Noir',
+      price: 7.99,
+      GST: 1.00,
+      posCategory: 'Wine',
+      GUID: '',
+      SKU: 'sku107',
+      selected: false,
+      quantity: 0
+    },
+    {
+      name: 'Merlot',
+      price: 7.99,
+      GST: 1.00,
+      posCategory: 'Wine',
+      GUID: '',
+      SKU: 'sku107',
+      selected: false,
+      quantity: 0
+    },
+    {
+      name: 'Chard',
+      price: 7.99,
+      GST: 1.00,
+      posCategory: 'Wine',
+      GUID: '',
+      SKU: 'sku107',
+      selected: false,
+      quantity: 0
+    },
+    {
+      name: 'Riesling',
+      price: 7.99,
+      GST: 1.00,
+      posCategory: 'Wine',
+      GUID: '',
+      SKU: 'sku107',
+      selected: false,
+      quantity: 0
+    },
+    {
+      name: 'Sauv blanc',
+      price: 7.99,
+      GST: 1.00,
+      posCategory: 'Wine',
+      GUID: '',
+      SKU: 'sku107',
+      selected: false,
+      quantity: 0
+    },
+    {
+      name: 'Pinot Grigio',
+      price: 7.99,
+      GST: 1.00,
+      posCategory: 'Wine',
+      GUID: '',
+      SKU: 'sku107',
+      selected: false,
+      quantity: 0
+    },
+    {
+      name: 'Unoaked Chardonnay',
+      price: 7.99,
+      GST: 1.00,
+      posCategory: 'Wine',
+      GUID: '',
+      SKU: 'sku107',
+      selected: false,
+      quantity: 0
+    },
+    {
+      name: 'Prosecco',
+      price: 7.99,
+      GST: 1.00,
+      posCategory: 'Happy Hour',
+      GUID: '',
+      SKU: 'sku107',
+      selected: false,
+      quantity: 0
+    },
+    {
+      name: 'Steak',
+      price: 7.99,
+      GST: 1.00,
+      posCategory: 'Food',
+      GUID: '',
+      SKU: 'sku107',
+      selected: false,
+      quantity: 0
+    },
+    {
+      name: 'Charcuterie',
+      price: 7.99,
+      GST: 1.00,
+      posCategory: 'Happy Hour',
+      GUID: '',
+      SKU: 'sku107',
       selected: false,
       quantity: 0
     },
@@ -78,7 +178,127 @@ export class ProductService {
       SKU: 'sku107',
       selected: false,
       quantity: 0
-    }
+    },
+    {
+      name: 'Fried Pickles',
+      price: 7.99,
+      GST: 1.00,
+      posCategory: 'Happy Hour',
+      GUID: '',
+      SKU: 'sku107',
+      selected: false,
+      quantity: 0
+    },
+    {
+      name: 'Pretzels',
+      price: 7.99,
+      GST: 1.00,
+      posCategory: 'Happy Hour',
+      GUID: '',
+      SKU: 'sku107',
+      selected: false,
+      quantity: 0
+    },
+    {
+      name: 'Wings',
+      price: 7.99,
+      GST: 1.00,
+      posCategory: 'Happy Hour',
+      GUID: '',
+      SKU: 'sku107',
+      selected: false,
+      quantity: 0
+    },
+    {
+      name: 'Cheese Board',
+      price: 7.99,
+      GST: 1.00,
+      posCategory: 'Happy Hour',
+      GUID: '',
+      SKU: 'sku107',
+      selected: false,
+      quantity: 0
+    },
+    {
+      name: 'Curly Fries',
+      price: 7.99,
+      GST: 1.00,
+      posCategory: 'Happy Hour',
+      GUID: '',
+      SKU: 'sku107',
+      selected: false,
+      quantity: 0
+    },
+    {
+      name: 'Yam Fries',
+      price: 7.99,
+      GST: 1.00,
+      posCategory: 'Happy Hour',
+      GUID: '',
+      SKU: 'sku107',
+      selected: false,
+      quantity: 0
+    },
+    {
+      name: 'Duck',
+      price: 7.99,
+      GST: 1.00,
+      posCategory: 'Food',
+      GUID: '',
+      SKU: 'sku107',
+      selected: false,
+      quantity: 0
+    },
+    {
+      name: 'Coke',
+      price: 7.99,
+      GST: 1.00,
+      posCategory: 'Beverage',
+      GUID: '',
+      SKU: 'sku107',
+      selected: false,
+      quantity: 0
+    },
+    {
+      name: 'Soda',
+      price: 7.99,
+      GST: 1.00,
+      posCategory: 'Beverage',
+      GUID: '',
+      SKU: 'sku107',
+      selected: false,
+      quantity: 0
+    },
+    {
+      name: 'N/A Beer',
+      price: 7.99,
+      GST: 1.00,
+      posCategory: 'Beverage',
+      GUID: '',
+      SKU: 'sku107',
+      selected: false,
+      quantity: 0
+    },
+    {
+      name: 'Event Tickets',
+      price: 7.99,
+      GST: 1.00,
+      posCategory: 'Happy Hour',
+      GUID: '',
+      SKU: 'sku107',
+      selected: false,
+      quantity: 0
+    },
+    {
+      name: 'Birthday Sundae',
+      price: 7.99,
+      GST: 1.00,
+      posCategory: 'Food',
+      GUID: '',
+      SKU: 'sku107',
+      selected: false,
+      quantity: 0
+    },
   ];
 
   public getAll(category?): any {
